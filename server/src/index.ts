63c181ff32d62c8ae17fb28a2856d6a441d6c12b
OM Thorat <omthorat1005@gmail.com>
2026-02-02 03:30:00 -0800

    change index file

---
+++ server/src/index.ts
@@ -0,0 +1,41 @@
+import express from "express"
+import cors from 'cors'
+import morgan from 'morgan'
+
+
+require("dotenv").config()
+
+const app = express()
+
+const MONGO_URL = process.env.MONGO_URL as string
+
+import authRouter from './routes/auth.route'
+import mongoose from "mongoose"
+
+app.use(express.json())
+app.use(cors())
+app.use(morgan('dev'))
+
+mongoose.connect(MONGO_URL)
+//routes
+app.use("/auth",authRouter)
+
+
+app.get('/',(req,res)=>{
+    res.status(200).json({"message":'Server Running!'})
+})
+
+
+app.get('/server-check',(req,res)=>{
+    let message="message"
+    setTimeout(()=>{
+        message="Server has started"
+    },3000)
+    return res.status(200).json({message})
+})
+
+
+app.listen(process.env.PORT,()=>{
+    console.log(process.env.PORT)
+    console.log("Server listening at 3000")
+})
